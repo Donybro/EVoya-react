1d307e9193753b009df01f0962cb18796718f17d
Donybro <doniyorrahmanov957@mail.ru>
2022-12-07 15:48:23 +0500

    in proccess ...

---
+++ app/components/CreateJuvenile/Defined/CreatePersonalInfo/CreatePersonalInfo.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react'
+import CreatePersonalInfoForm from "./CreatePersonalInfoForm";
+
+const CreatePersonalInfo: FC = () => {
+    return <div>CreatePersonalInfo
+    <CreatePersonalInfoForm/>
+    </div>
+}
+
+export default CreatePersonalInfo
